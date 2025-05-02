376bb70572b91a335d22b60b98153c75f1d45df1
bvlion <bvltiggeari@ambitious-i.net>
2025-05-02 09:41:33 +0900

    update firebase actions

--- node/functions/index.mjs
+++ node/functions/index.mjs
@@ -1,18 +1,7 @@
 import { onRequest } from "firebase-functions/v2/https"
-import * as logger from "firebase-functions/logger"
 import gplay from 'google-play-scraper'
 
-const validToken = process.env.BEARER_TOKEN
-
 export const scrape = onRequest(async (request, response) => {
-  const authHeader = request.headers['authorization'] || ''
-  const token = authHeader.replace(/^Bearer\s+/i, '')
-
-  if (token !== validToken) {
-    logger.error('Unauthorized access attempt', { validToken })
-    return response.status(401).json({ error: 'Unauthorized' })
-  }
-
   const packageName = request.query.packageName || request.body?.packageName
   if (!packageName) {
     return response.status(400).json({ error: 'Missing Parameter' })
